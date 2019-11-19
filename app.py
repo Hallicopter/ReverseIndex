2c957fca6ee419c852ca6f3b268c1bdc5ad1530b
Advait Raykar <advait.rayjkar@gmail.com>
2019-11-19 20:06:40 +0530

    disg code

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from elasticSearch import elasticSearch
 import pprint
-import pdf2txt
+# import pdf2txt
 import pdfkit
 
 from flask import Flask, render_template, flash, request, send_file
@@ -111,7 +111,7 @@ def pdfElasticSearch():
         pretty+='</ol>'
         pretty+='<a href="/pdf">Search again!</a>'
         pretty+='<br><a href="/out">Download output.</a>'
-        pdfkit.from_string(output, "/tmp/out.pdf")
+        pdfkit.from_string(output, "out.pdf")
         return pretty
 
     elif request.form['singlebutton'] == 'clear':
@@ -124,7 +124,7 @@ def pdfElasticSearch():
 @app.route('/out')
 def return_files_tut():
 	try:
-		return send_file('/tmp/out.pdf', attachment_filename='out.pdf')
+		return send_file('out.pdf', attachment_filename='out.pdf')
 	except Exception as e:
 		return str(e)
     
@@ -136,3 +136,31 @@ if __name__ == '__main__':
 
 
 
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from io import StringIO
+
+def convert_pdf_to_txt(path):
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, laparams=laparams)
+    fp = open(path, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos=set()
+
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
+        interpreter.process_page(page)
+
+    text = retstr.getvalue()
+
+    fp.close()
+    device.close()
+    retstr.close()
+    return text
