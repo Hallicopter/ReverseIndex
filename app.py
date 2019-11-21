a4339e047bf6888faa5ff23a1bdba3deded0a62e
Advait Raykar <advait.rayjkar@gmail.com>
2019-11-21 23:20:07 +0530

    pdf parsing

--- app.py
+++ app.py
@@ -2,10 +2,36 @@ from elasticSearch import elasticSearch
 import pprint
 # import pdf2txt
 import pdfkit
-
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from io import StringIO
 from flask import Flask, render_template, flash, request, send_file
 # from flask import Flask
 
+def convert_pdf_to_txt(path):
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, laparams=laparams)
+    fp = open(path, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos=set()
+
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
+        interpreter.process_page(page)
+
+    text = retstr.getvalue()
+
+    fp.close()
+    device.close()
+    retstr.close()
+    return text
 
 def merge_dict(d1, d2):
     
@@ -93,54 +119,66 @@ def pdfPage():
 @app.route('/pdf', methods=['POST'])
 def pdfElasticSearch():
     global es
+    # if request.form['singlebutton'] == 'getInput':
+        
     if 'singlebutton' not in request.form:
         pdfFileObj = request.files['file']
         pdfFileObj.save(pdfFileObj.filename)
         text = convert_pdf_to_txt(pdfFileObj.filename)
-        print(text)
-        es = elasticSearch()
-        es.raw_text = text
-    elif request.form['singlebutton'] == 'indexDoc':
-        try:
-            es==None
-        except:
-            return render_template('formPDF.html') + \
-            '<script>alert("Please enter text first!");</script>'
-
-        es.convertToDocs()
-        for d in es.doc_data:
-            es.indexDoc(d)
+        
+        if es!=None:
+            es_tmp = elasticSearch()
+            es_tmp.raw_text = text
+            es_tmp.convertToDocs()
+            for d in es_tmp.doc_data:
+                es_tmp.indexDoc(d)
+            # es.invertedIndex.update(es_tmp.invertedIndex)
+            es.invertedIndex = merge_dict(es.invertedIndex, es_tmp.invertedIndex)
+            # inv_es = {v: k for k, v in my_map.items()}
+            es.doc_data.update(es_tmp.doc_data)
+        else:
+            es = elasticSearch()
+            es.raw_text = text
+            es.convertToDocs()
+            for d in es.doc_data:
+                es.indexDoc(d)
+        
+        print(es.invertedIndex)
+        
+        
        
     elif request.form['singlebutton'] == 'search':
-        try:
-            es==None
-        except:
+        
+        if es==None:
+            
+       
+            print('No es there')
             return render_template('formPDF.html') + \
             '<script>alert("Please enter text first!");</script>'
 
-        if es.invertedIndex  == {}:
-            return render_template('formPDF.html') + \
-            '<script>alert("Please index first!");</script>'
-            
-        paras = es.search(request.form['searchterm'])
+        else:
+            print('wooot', es.invertedIndex)
+            paras = es.search(request.form['searchterm'])
 
-        if paras == None:
-            return render_template('formPDF.html') + \
-            '<script>alert("Word not found!");</script>'
-        
-        output = ''
-        pretty = '<ol>'
-        for para in paras:
-            output += ' ' + para[:] + '\n \n'
-            pretty+='<li>' + para + "</li>"
-        pretty+='</ol>'
-        pretty+='<a href="/pdf">Search again!</a>'
-        pretty+='<br><a href="/out">Download output.</a>'
-        # pdfkit.from_string(output, "out.pdf")
-        return pretty
+            if paras == None:
+                return render_template('formPDF.html') + \
+                '<script>alert("Word not found!");</script>'
+
+            pretty = '<ol>'
+            output = ''
+            for para in paras:
+                output+= "\n\n" + para[:]
+                pretty+='<li>' + para + "</li>"
+                  
+            pretty+='</ol>'
+            pretty+='<a href="/pdf">Search again!</a>'
+            pretty+='<br><a href="/out">Download output.</a>'
+            pdfkit.from_string(output, "out.pdf")
+
+            return pretty
 
     elif request.form['singlebutton'] == 'clear':
-        es = elasticSearch()
+        es = None
         return render_template('formPDF.html') + \
             '<script>alert("Cleared Index!");</script>'
 
@@ -161,33 +199,10 @@ if __name__ == '__main__':
 
 
 
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
-from pdfminer.pdfpage import PDFPage
-from io import StringIO
 
 
 
-def convert_pdf_to_txt(path):
-    rsrcmgr = PDFResourceManager()
-    retstr = StringIO()
-    codec = 'utf-8'
-    laparams = LAParams()
-    device = TextConverter(rsrcmgr, retstr, laparams=laparams)
-    fp = open(path, 'rb')
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
-    password = ""
-    maxpages = 0
-    caching = True
-    pagenos=set()
 
-    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
-        interpreter.process_page(page)
-
-    text = retstr.getvalue()
-
-    fp.close()
-    device.close()
-    retstr.close()
-    return text
+pdfFileObj = request.files['file']
+pdfFileObj.save(pdfFileObj.filename)
+text = convert_pdf_to_txt(pdfFileObj.filename)
